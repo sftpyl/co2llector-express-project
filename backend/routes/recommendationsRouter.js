e3b25a3892b2ba9a271bdb6fe0753906696040c1
Rocio Tellez Lopez <rociotellezlopez@gmail.com>
2025-04-23 10:05:39 -0500

    fix: modifica respuesta de openAi en formato json

--- backend/routes/recommendationsRouter.js
+++ backend/routes/recommendationsRouter.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 const PATH_RECOMMENDATIONS = '/recommendations';
 
-router.post(`${PATH_RECOMMENDATIONS}`, recommendations());
+router.post(`${PATH_RECOMMENDATIONS}`, recommendations);
 
 module.exports = router;
--- backend/services/openAiService.js
+++ backend/services/openAiService.js
@@ -13,19 +13,37 @@ const generateResponse = async (company, totalEmision, details) => {
   Asegúrate de que el mensaje sea claro y fácil de entender para el usuario.
   El mensaje debe ser amigable y alentador, y debe motivar a la empresa a tomar medidas para reducir su huella de carbono,
   además debe incluir un resumen de las emisiones y un llamado a la acción para que la empresa implemente las recomendaciones.
+  
+  Tu tarea es generar una recomendación para reducir esa huella, pero **respondé únicamente en formato JSON** con esta estructura:
+
+  {
+    "empresa": "nombre",
+    "huella_total": total,
+    "top_emisiones": ["actividad1", "actividad2", ...],
+    "recomendaciones": ["sugerencia1", "sugerencia2", ...],
+    "mensaje_final": "frase motivacional o cierre profesional"
+  }
+
+  No agregues texto fuera del JSON.
+
   `;
 
   const response = await openai.chat.completions.create({
-    model: "gpt-4-mini",
+    model: "gpt-4o-mini",
     messages: [{ role: "user", content: prompt }],
     max_tokens: 500,
     temperature: 0.7,
   });
 
-  console.log("Response from OpenAI:", response);
-  return response.choices[0].message.content;
+  // console.log("Response from OpenAI:", JSON.parse(response.choices[0].message.content));
+  const respuesta = JSON.parse(response.choices[0].message.content);
+
+  // console.log("Response from OpenAI:", respuesta);
+  
+  // return response.choices[0].message.content;
+  return respuesta;
 };
 
 module.exports = {
   generateResponse,
-}
+};
