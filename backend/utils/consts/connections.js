48beabecfd1dcc3a0e61b9c82707a0b9af731fcd
Soft P&L <soft.pyl0800@gmail.com>
2025-04-24 18:05:33 -0500

    Update connections.js

--- backend/utils/consts/connections.js
+++ backend/utils/consts/connections.js
@@ -1,12 +1,17 @@
 //MENSAJES DE EXITO PARA MANEJO DE LAS CONECCIONES
-export const MESSAGE_CORRECT_CONECTION = {
+const MESSAGE_CORRECT_CONECTION = {
   MONGO: "Conectado a MongoDB correctamente",
   SERVER: "Servidor corriendo en el puerto:",
 
 };
 
 //MENSAJES DE ERROR PARA MANEJO DE LAS CONECCIONES
-export const MESSAGE_ERROR_CONECTION = {
+const MESSAGE_ERROR_CONECTION = {
   MONGO: "Error al conectar a MongoDB:",
   SERVER: "Error al iniciar el servidor:",
 }
+
+module.exports = {
+  MESSAGE_CORRECT_CONECTION,
+  MESSAGE_ERROR_CONECTION,
+};
